a12cb7d0c3989cfd68b3d827e2fe77f7a4b75ef6
Jaspreet Singh <sandhujassi088@gamil.com>
2023-03-18 11:35:43 +0530

    debugging

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'normalize.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'src/assets/css/styles.css';
+import '../src/assets/css/styles.css';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
 
